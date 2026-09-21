80d47a66e7db947e19a0d5a875fa17d9ff472744
forax <forax@univ-mlv.fr>
2026-09-21 23:48:10 +0200

    simplify imports

--- injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
+++ injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
@@ -1,31 +1,13 @@
 package com.github.forax.framework.injector;
 
-import com.sun.source.tree.Tree;
-
 import java.beans.PropertyDescriptor;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.lang.module.ModuleFinder;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Spliterators;
-import java.util.TreeSet;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static java.util.Objects.requireNonNull;
 
